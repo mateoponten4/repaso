86cb64aa13ac5a9f75aeec403aba1c21d7284f28
mateoponten4 <mateoponcedeleon336@gmail.com>
2025-03-10 11:43:14 -0300

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Repaso
-## Ejercicio en python
---
+++ README.py
@@ -0,0 +1,4 @@
+# Repaso
+## Ejercicio en python
+palabra = input("Ingrese una palabra porfavor")
+print = ("La palabra que ingresaste es: ", palabra)
