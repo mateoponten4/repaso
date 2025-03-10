0910a383612fd1be122fb113cbf97d8436be3bf5
mateoponten4 <mateoponcedeleon336@gmail.com>
2025-03-10 12:42:08 -0300

    Update README.py

--- README.py
+++ README.py
@@ -2,7 +2,7 @@
 ## Ejercicio en python
 palabra = input("Ingrese una palabra porfavor")
 print = ("La palabra que ingresaste es: ", palabra)
-
+#crear un programa que pida ingresar un numero entero, si el numero es mayor a diez mostrar por pantalla 10 si el numero es menor a 10 mostrar por pantalla fin del programa
 numero = int("ingrese un numero entero: ")
 if numero > 10 print("10")
 else print("fin del programa")
